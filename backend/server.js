874768175198cbe6a4c025025e9996a028c3f1da
Gregory Monroe II <gregthemusician@gmail.com>
2022-10-23 21:03:31 -0400

    define app to use express and incorporate packages through that

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,14 @@ const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 
+require('dotenv').config()
+
+//app
+
+const app = express()
+
+//middlewares
+app.use(morgan('dev))
+app.use(bodyParser.json())
+app.use(cookieParser())
 
