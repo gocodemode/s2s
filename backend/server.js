ed16f44a2a426fcd5d977f65b33a282ef0306a93
Gregory Monroe II <gregthemusician@gmail.com>
2022-10-23 21:55:44 -0400

    removed typos. now server is running and db is connected

--- backend/server.js
+++ backend/server.js
@@ -30,9 +30,8 @@ app.get('/api', (req, res) => {
 //database connection
 mongoose
     .connect(process.env.MONGO_URI, {useNewUrlParser: true})
-    .then(() => console.log("DB Connected"));
+    .then(() => console.log("DB Connected"))
     .catch((err) => console.log("DB Error => ", err));
-});
 
 //port
 const port = process.env.PORT || 8000
