a48fba1e80267581b68135a07b7c30ea7655eba5
Gregory Monroe II <gregthemusician@gmail.com>
2022-10-23 21:52:14 -0400

    correct mongoose.connection.on to a .catch instead

--- backend/server.js
+++ backend/server.js
@@ -31,9 +31,7 @@ app.get('/api', (req, res) => {
 mongoose
     .connect(process.env.MONGO_URI, {useNewUrlParser: true})
     .then(() => console.log("DB Connected"));
-
-mongoose.connection.on("error", err => {
-    console.log(`DB connection error: ${err.message}`);
+    .catch((err) => console.log("DB Error => ", err));
 });
 
 //port
